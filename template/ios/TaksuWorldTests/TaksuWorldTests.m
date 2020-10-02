8dff0352b254fbc13557c8cb1f579b384ccf832b
Reroet <wayanpuguhsudarma@gmail.com>
2020-10-02 16:26:05 +0800

    update template

--- template/ios/TaksuWorldTests/TaksuWorldTests.m
+++ template/ios/TaksuWorldTests/TaksuWorldTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface taksuTests : XCTestCase
+@interface TaksuWorldTests : XCTestCase
 
 @end
 
-@implementation taksuTests
+@implementation TaksuWorldTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
