b7133a97e46e8c3f07e7b0155feb7e46965e8b97
Reroet <wayanpuguhsudarma@gmail.com>
2020-10-02 15:08:17 +0800

    update template config

--- template.config.js
+++ template.config.js
@@ -1,11 +1,14 @@
 module.exports = {
-  // Placeholder used to rename and replace in files
-  // package.json, index.json, android/, ios/
+  // Placeholder name that will be replaced in package.json, index.json, android/, ios/ for a project name.
   placeholderName: "taksu",
 
-  // Directory with template
+  // Placeholder title that will be replaced in values.xml and Info.plist with title provided by the user.
+  // We default this value to 'Hello App Display Name', which is default placeholder in react-native template.
+  titlePlaceholder: "Taksu Mobile App",
+
+  // Directory with the template which will be copied and processed by React Native CLI. Template directory should have package.json with all dependencies specified, including `react-native`.
   templateDir: "./template",
 
-  // Path to script, which will be executed after init
-  // postInitScript: "./script.js",
+  // Path to script, which will be executed after initialization process, but before installing all the dependencies specified in the template. This script runs as a shell script but you can change that (e.g. to Node) by using a shebang (see example custom template).
+  postInitScript: "./script.js",
 };
