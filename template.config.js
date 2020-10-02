a111bed3793c41321d00027fa86c23b1e5eb5d44
Reroet <wayanpuguhsudarma@gmail.com>
2020-10-02 16:33:39 +0800

    template

--- template.config.js
+++ template.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   // Placeholder name that will be replaced in package.json, index.json, android/, ios/ for a project name.
-  placeholderName: "taksu",
+  placeholderName: "TaksuWorld",
 
   // Placeholder title that will be replaced in values.xml and Info.plist with title provided by the user.
   // We default this value to 'Hello App Display Name', which is default placeholder in react-native template.
-  titlePlaceholder: "Taksu Mobile App",
+  titlePlaceholder: "TaksuWorld",
 
   // Directory with the template which will be copied and processed by React Native CLI. Template directory should have package.json with all dependencies specified, including `react-native`.
   templateDir: "./template",
